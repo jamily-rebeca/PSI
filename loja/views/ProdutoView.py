b1e7d1702280af62bc1a9218cde9809191673fbe
jamily-rebeca <rebeca.pereira@escolar.ifrn.edu.br>
2025-05-12 14:22:58 -0300

    mais caminhosss

---
+++ loja/views/ProdutoView.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+def list_produto_view(request, id=None):
+
+    return HttpResponse('<h1>Produto de id %s!</h1>' % id)
--- lojaAdmin/urls.py
+++ lojaAdmin/urls.py
@@ -20,8 +20,12 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.conf.urls import include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('loja.urls.HomeUrls')),
+    path('produto/', include('loja.urls.ProdutoUrls')),
 ]
 urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
 urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
