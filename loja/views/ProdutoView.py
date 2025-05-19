ba73124d6b5aa015da8438e823343ccccbb29c99
jamily-rebeca <rebeca.pereira@escolar.ifrn.edu.br>
2025-05-19 14:28:52 -0300

    pesquisa

--- loja/views/ProdutoView.py
+++ loja/views/ProdutoView.py
@@ -1,4 +1,44 @@
 from django.http import HttpResponse
+from loja.models import Produto
 def list_produto_view(request, id=None):
+    #carrega dados do navegador
+    produto = request.GET.get("produto")
+    destaque = request.GET.get("destaque")
+    promocao = request.GET.get("promocao")
+    categoria = request.GET.get("categoria")
+    fabricante = request.GET.get("fabricante")
+#mostra dados do navegador 
+    if destaque is not None:
+        print(destaque)
+    if produto is not None:
+        print(produto)
+    if promocao is not None:
+        print(promocao)
+    if categoria is not None:
+        print(categoria)
+    if fabricante is not None:
+        print(fabricante)
+    #carregada dados do banco de dados 
 
+    #produtos = Produto.objects.all()
+    #mostra dados do banco de dados 
+    #print(produtos)
+
+    produtos = Produto.objects.all()
+    if produto is not None:
+        produtos = produtos.filter(Produto=produto)
+    if promocao is not None:
+        produtos = produtos.filter(promocao=promocao)
+    if destaque is not None:
+        produtos = produtos.filter(destaque=destaque)
+    if categoria is not None:
+        produtos = produtos.filter(categoria=categoria)
+    if fabricante is not None:
+        produtos = produtos.filter(fabricante=fabricante)
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    print(produtos)
+
+    if id is None:
+        return HttpResponse('<h1>Nenhum id foi informado</h1>')
     return HttpResponse('<h1>Produto de id %s!</h1>' % id)
