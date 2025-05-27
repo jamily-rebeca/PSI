462c5f1ba9ce94e7b50e2e3947353d53553ff300
jamily-rebeca <rebeca.pereira@escolar.ifrn.edu.br>
2025-05-27 15:44:42 -0300

    inserindo html

--- loja/views/HomeView.py
+++ loja/views/HomeView.py
@@ -1,3 +1,8 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
+#def home_view(request):
+ #   return HttpResponse('<h1>Olá mundo!</h1>')
+
+
+from django.shortcuts import render
 def home_view(request):
-    return HttpResponse('<h1>Olá mundo!</h1>')
+    return render(request, template_name='home/home.html', status=200)
