b91d02d739151ac6dad94b26a3b3446a1499557b
jamily-rebeca <rebeca.pereira@escolar.ifrn.edu.br>
2025-06-02 14:20:38 -0300

    com html

--- loja/views/HomeView.py
+++ loja/views/HomeView.py
@@ -1,8 +1,18 @@
-#from django.http import HttpResponse
-#def home_view(request):
- #   return HttpResponse('<h1>Olá mundo!</h1>')
+# from django.http import HttpResponse
+# def home_view(request):
+#   return HttpResponse('<h1>Olá mundo!</h1>')
 
 
 from django.shortcuts import render
+from loja.models import Produto
+
+
 def home_view(request):
-    return render(request, template_name='home/home.html', status=200)
+    produto = request.GET.get("produto")
+    produtos = Produto.objects.all()
+    if produto is not None:
+        produtos = produtos.filter(Produto__contains=produto)
+    print(produto)#conteúdo digitado pelo usuário 
+    print(produtos) #Produtos do banco de dados com o texto digitado pelo usuário
+    context = {"produtos": produtos}
+    return render(request, template_name="home/home.html", context=context, status=200)
