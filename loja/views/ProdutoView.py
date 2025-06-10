1b4e67caeacce42e77f911ae535770af9716db7c
jamily-rebeca <rebeca.pereira@escolar.ifrn.edu.br>
2025-06-10 15:49:04 -0300

    edição de produtos

--- loja/views/ProdutoView.py
+++ loja/views/ProdutoView.py
@@ -1,7 +1,48 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
+from django.shortcuts import render, redirect # Retire from django.http import HttpResponse
+from loja.models import Produto
+
+
+def edit_produto_postback(request, id=None):
+    # Processa o post back gerado pela action
+    if request.method == 'POST':
+        # Salva dados editados
+        id = request.POST.get("id")
+        produto = request.POST.get("Produto")
+        destaque = request.POST.get("destaque")
+        promocao = request.POST.get("promocao")
+        msgPromocao = request.POST.get("msgPromocao")
+        print("postback")
+        print(id)
+        print(produto)
+        print(destaque)
+        print(promocao)
+        print(msgPromocao)
+        try:
+            obj_produto = Produto.objects.filter(id=id).first()
+            obj_produto.Produto = produto
+            obj_produto.destaque = (destaque is not None)
+            obj_produto.promocao = (promocao is not None)
+            if msgPromocao is not None:
+                obj_produto.msgPromocao = msgPromocao
+                obj_produto.save()
+                print("Produto %s salvo com sucesso" % produto)
+        except Exception as e:
+            print("Erro salvando edição de produto: %s" % e)
+    return redirect("/produto")
+
+
+def edit_produto_view(request, id=None):
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = { 'produto': produto }
+    return render(request, template_name='produto/produto-edit.html', context=context, status=200)
 
 def list_produto_view(request, id=None):
     #carrega dados do navegador
@@ -12,6 +53,7 @@ def list_produto_view(request, id=None):
     fabricante = request.GET.get("fabricante")
 
     dias = request.GET.get("dias")
+    produtos = Produto.objects.all()
 #mostra dados do navegador 
     if destaque is not None:
         print(destaque)
@@ -35,6 +77,12 @@ def list_produto_view(request, id=None):
         now = timezone.now()
         now = now - timedelta(days = int(dias))
         produtos = produtos.filter(criado_em__gte=now)
+    context = {'produtos': produtos}
+    return render(request, template_name='produto/produto.html', context=context, status=200)
+
+    
+
+
 
     if produto is not None:
         produtos = produtos.filter(Produto__icontains=produto )
